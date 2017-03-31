a68d19a9bfddc868e9b9c5f59a750952e2134b92
Shayne Koestler <shayne.koestler@kineticdata.com>
2017-03-31 15:56:26 -0500

    added fetch, Object.assign, and Promise polyfills, fixed production webpack for bootstrap loaders

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+require('es6-object-assign').polyfill();
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
 import 'bootstrap/dist/css/bootstrap.css';
 import { AppContainer } from 'react-hot-loader';
 import React from 'react';
