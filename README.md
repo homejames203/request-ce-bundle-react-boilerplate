0012ca9aec3854afa244640f6b1e11fbf3883e75
Benjamin Christenson <ben.christenson@kineticdata.com>
2017-04-20 15:42:42 -0500

    Changing some code and directory structure to simplify, improve consistency, and help to navigate to the more important files.

---
+++ README.md
@@ -0,0 +1,39 @@
+# request-ce-bundle-react
+
+This is a boilerplate project for building Kinetic Request CE client side
+bundles in React.
+
+You will need to have nodejs and yarn (a nodejs package manager) running
+locally, and an instance of Kinetic Request CE running with the
+[request-ce-bundle-webpack](https://github.com/kineticdata/request-ce-bundle-webpack)
+bundle deployed.
+
+## Quickstart
+* Modify the **bundleName**, **localPort**, and **kineticWebserver**
+  configuration setting in the `config.js` file (other configuration settings
+  are relatively universal).
+* Configure your space or kapp to use the **request-ce-bundle-webpack** bundle
+  and set the the display page to be **webpack.jsp?bundleName=BUNDLE_NAME**
+  (where **BUNDLE_NAME** is equal to the value specified in the `/config.js`
+  file).
+* Run `yarn install` and `yarn start`.
+* Open **http://localhost:LOCAL_PORT/kinetic/SPACE/KAPP** in your web browser
+  *(http://localhost:LOCAL_PORT/SPACE/KAPP if you are connecting to kinops.io)*.
+
+## Command Reference
+* `yarn start` will run the project in development mode (requires Kinetic
+  Request CE to be running)
+* `yarn run build` will build the project in "production" mode (this will create
+  a *dist* directory with all of the static files necessary to deploy the
+  project)
+* `yarn add LIBRARY` will install a library and update the *package.json* file.
+
+## Deploying into Production Mode
+In order to expose the project to others, the static files will need to be
+accessible (Amazon s3 is often a convenient and simple way to serve the files).
+
+One the static files have been deployed somewhere, the space or kapp display
+page can be set to **webpack.jsp?bundleName=BUNDLE_NAME&location=LOCATION**
+(where **BUNDLE_NAME** is equal to the value specified in the `/config.js` file
+and **LOCATION** is equal to the location that the files are served from, such
+as *https://s3.amazonaws.com/acme.com/bundles/catalog*.).
--- src/bundle/index.js
+++ src/bundle/index.js
@@ -1,40 +1,56 @@
+/**
+ * README - ...
+ */
+
+// Loading global polyfills
 require('es6-object-assign').polyfill();
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
-import 'bootstrap/dist/css/bootstrap.css';
+// Importing libraries
 import { AppContainer } from 'react-hot-loader';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
 import { compose, createStore, combineReducers, applyMiddleware } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import { reducer as formReducer } from 'redux-form';
 import { createHashHistory } from 'history';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
 
-import App from './containers/App';
-import reducers from './redux/reducers';
+// Importing bundle files
+import App from '../containers/App';
+import reducers from '../redux/reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// Preparing history and redux store using the 'history' and 'redux' libraries
 const history = createHashHistory();
-
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   connectRouter(history)(combineReducers({ ...reducers, form: formReducer })),
   composeEnhancers(applyMiddleware(routerMiddleware(history), promiseMiddleware))
 );
 
+// Rendering the application
 const rootEl = document.getElementById('root');
 const render = Component =>
   ReactDOM.render(
     <AppContainer>
-      <Component store={store} history={history} />
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Route path="/" component={Component}/>
+        </ConnectedRouter>
+      </Provider>
     </AppContainer>,
     rootEl
   );
-
 render(App);
+
+// Enable hot module replacement so that file changes are automatically
+// communicated to the browser when running in development mode
 if (module.hot) {
-  module.hot.accept('./containers/App', () => render(App));
-  module.hot.accept('./redux/reducers', () =>
+  module.hot.accept('../containers/App', () => render(App));
+  module.hot.accept('../redux/reducers', () =>
     store.replaceReducer(connectRouter(history)(combineReducers(reducers))));
 }
