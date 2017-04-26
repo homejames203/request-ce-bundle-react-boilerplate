faa4689fa0ab95b4b8b7db6644023cd11d75e628
Shayne Koestler <shayne.koestler@kineticdata.com>
2017-04-26 16:22:38 -0500

    adds support for sass files

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,10 @@ module.exports = {
         use: [ 'style-loader', 'css-loader' ]
       },
       {
+        test: /\.scss$/,
+        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+      },
+      {
         test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
         use: 'url-loader?limit=10000&mimetype=application/font-woff'
       }, {
